8d5218afa32d61e99a42c2ed69458f858f43e9a5
kenta <k-yamasaki@bps21.jp>
2021-07-27 09:27:21 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { default as DifferenceHashBuilder } from './DifferenceHashBuilder';
 import { default as HexadecimalToHash } from './HexadecimalToHash';
 import { default as Hash } from './Hash';
+import { lanczos } from '@rgba-image/lanczos';
 
 interface globalThis {
     hash: any,
@@ -11,4 +12,5 @@ global.hash = {
     DifferenceHashBuilder,
     HexadecimalToHash,
     Hash,
+    lanczos,
 }
